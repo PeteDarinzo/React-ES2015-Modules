f9b9fb0327c51cc3ede963c0d1af624f6bc7c32b
Peter Darinzo <birel44@yahoo.com>
2021-12-22 19:33:17 -0700

    withdraw all fruits

--- src/index.js
+++ src/index.js
@@ -2,7 +2,105 @@ import { choice, remove } from './helpers';
 
 import fruits from './foods';
 
-const selection = choice(fruits);
+let selection = choice(fruits);
+console.log(`I'd like one ${selection}, please.`);
+console.log(`Here you go: ${selection}`);
+console.log("Delicious! May I have another?");
+remove(fruits, selection);
+console.log(`I'm sorry, we're all out. We have ${fruits.length} left.`);
+
+selection = choice(fruits);
+console.log(`I'd like one ${selection}, please.`);
+console.log(`Here you go: ${selection}`);
+console.log("Delicious! May I have another?");
+remove(fruits, selection);
+console.log(`I'm sorry, we're all out. We have ${fruits.length} left.`);
+
+selection = choice(fruits);
+console.log(`I'd like one ${selection}, please.`);
+console.log(`Here you go: ${selection}`);
+console.log("Delicious! May I have another?");
+remove(fruits, selection);
+console.log(`I'm sorry, we're all out. We have ${fruits.length} left.`);
+
+selection = choice(fruits);
+console.log(`I'd like one ${selection}, please.`);
+console.log(`Here you go: ${selection}`);
+console.log("Delicious! May I have another?");
+remove(fruits, selection);
+console.log(`I'm sorry, we're all out. We have ${fruits.length} left.`);
+
+selection = choice(fruits);
+console.log(`I'd like one ${selection}, please.`);
+console.log(`Here you go: ${selection}`);
+console.log("Delicious! May I have another?");
+remove(fruits, selection);
+console.log(`I'm sorry, we're all out. We have ${fruits.length} left.`);
+
+selection = choice(fruits);
+console.log(`I'd like one ${selection}, please.`);
+console.log(`Here you go: ${selection}`);
+console.log("Delicious! May I have another?");
+remove(fruits, selection);
+console.log(`I'm sorry, we're all out. We have ${fruits.length} left.`);
+
+selection = choice(fruits);
+console.log(`I'd like one ${selection}, please.`);
+console.log(`Here you go: ${selection}`);
+console.log("Delicious! May I have another?");
+remove(fruits, selection);
+console.log(`I'm sorry, we're all out. We have ${fruits.length} left.`);
+
+selection = choice(fruits);
+console.log(`I'd like one ${selection}, please.`);
+console.log(`Here you go: ${selection}`);
+console.log("Delicious! May I have another?");
+remove(fruits, selection);
+console.log(`I'm sorry, we're all out. We have ${fruits.length} left.`);
+
+selection = choice(fruits);
+console.log(`I'd like one ${selection}, please.`);
+console.log(`Here you go: ${selection}`);
+console.log("Delicious! May I have another?");
+remove(fruits, selection);
+console.log(`I'm sorry, we're all out. We have ${fruits.length} left.`);
+
+selection = choice(fruits);
+console.log(`I'd like one ${selection}, please.`);
+console.log(`Here you go: ${selection}`);
+console.log("Delicious! May I have another?");
+remove(fruits, selection);
+console.log(`I'm sorry, we're all out. We have ${fruits.length} left.`);
+
+selection = choice(fruits);
+console.log(`I'd like one ${selection}, please.`);
+console.log(`Here you go: ${selection}`);
+console.log("Delicious! May I have another?");
+remove(fruits, selection);
+console.log(`I'm sorry, we're all out. We have ${fruits.length} left.`);
+
+selection = choice(fruits);
+console.log(`I'd like one ${selection}, please.`);
+console.log(`Here you go: ${selection}`);
+console.log("Delicious! May I have another?");
+remove(fruits, selection);
+console.log(`I'm sorry, we're all out. We have ${fruits.length} left.`);
+
+selection = choice(fruits);
+console.log(`I'd like one ${selection}, please.`);
+console.log(`Here you go: ${selection}`);
+console.log("Delicious! May I have another?");
+remove(fruits, selection);
+console.log(`I'm sorry, we're all out. We have ${fruits.length} left.`);
+
+selection = choice(fruits);
+console.log(`I'd like one ${selection}, please.`);
+console.log(`Here you go: ${selection}`);
+console.log("Delicious! May I have another?");
+remove(fruits, selection);
+console.log(`I'm sorry, we're all out. We have ${fruits.length} left.`);
+
+selection = choice(fruits);
 console.log(`I'd like one ${selection}, please.`);
 console.log(`Here you go: ${selection}`);
 console.log("Delicious! May I have another?");
